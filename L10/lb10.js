58d7eec21f025bc953ca114b525283980bdf0b49
Muhammad B Khan <muhammad.khan33@student.qcc.cuny.edu>
2021-03-06 02:33:28 +0000

    Modified lab to file and added css and js files to it

---
+++ L10/lb10.js
@@ -0,0 +1,6 @@
+/* JavaScript programs the behavior of web pages */
+function changeTxt() {
+  document.getElementById("chng").innerHTML = "The text have changed after a click on the button.";
+}
+
+changeTxt();
