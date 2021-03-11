feced06c8981760ca28e694d47eea7558c5a2517
Muhammad B Khan <muhammad.khan33@student.qcc.cuny.edu>
2021-03-11 01:01:18 +0000

    Added lists of strings, numbers, and both for homework.

---
+++ L11/lab11.js
@@ -0,0 +1,62 @@
+var num1 = 10;
+var num2 = 21;
+var num3 = 7;
+var num4 = 12;
+var age = num1 + num4
+
+var myName = "Muhammad";
+var answer = "Hello!";
+var price = "Priceless";
+var myCar = "bmw";
+
+document.getElementById("demo").innerHTML = answer +" "+ myName;
+document.getElementById("age").innerHTML = "I am " + age;
+
+var zow;
+var fly;
+fly = "to" + "day";
+zow = 4 - 1;
+fly = 3 * 3;
+zow = 4 + "now;"
+
+var fuzz;
+var clip;
+fuzz = fuzz + 2;
+fuzz = clip + 1;
+clip = "gr" + fuzz;
+fuzz = fuzz + 1;
+fuzz = fuzz + 1;
+fuzz = fuzz + 1;
+
+3 < 8 = true;
+
+var myScore;
+var myLives;
+if myScore * myLives > 10;
+return true;
+
+var humanYear = 1;
+var dogYear = humanYear * 7;
+if (var dog == humanYear) {
+  return true
+};
+
+
+var myMoney;
+var myAge;
+if (myMoney == 40 && myAge > 14) {
+  return true;
+}
+
+
+/* creating three lists for hw */
+var thisYear = 2021;
+var season = "Spring";
+
+var myGrades = ["Math", 95, "ET705", 92, "MicroEcon", 96];
+
+var ages = [21, 24, 26, 19, 24, 23];
+var names = ["Mike", "Muhammad" "Doe", "Jane", "John"];
+
+var mySelf = {firstn: "Muhammad", lastn: "Khan", myGrade: 100};
+mySelf[2] = 98;
